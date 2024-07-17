eeaafe1e15f5bc626658a67c7b8cf9cb99efac56
Lucia Mendes <luciasofiamendes@gmail.com>
2024-07-17 18:56:13 +0100

    created a file to test the UI and interact with the  nodes feature

--- cypress/e2e/nodesUI.spec.cy.ts
+++ cypress/e2e/nodesUI.spec.cy.ts
@@ -8,17 +8,21 @@ const username = Cypress.env("username")
 const password = Cypress.env("password")
 
 describe('Positive UI tests to the Nodes page', () => {
+
     before('Login to Moralis Admin', () => {
+
+        cy.clearLocalStorage();
         GlobalCommands.goTo(url)
 
         cy.get('#cookiescript_accept').click()
 
+        //TODO: parametrize elements
         cy.get('[id="admin-login-email"]').focus().type(username)
         cy.get('[id="admin-login-password"]').focus().type(password)
         cy.get('[data-testid="test-button"]').click()
 
         //Validate the Login was successful
-        cy.get('[data-testid="test-typography"]').contains('Welcome')
+        cy.get('[data-testid="test-typography"]').contains('Welcome').should('be.visible')
     })
     it('Create a new Node', () => {
 
@@ -29,11 +33,10 @@ describe('Positive UI tests to the Nodes page', () => {
             cy.get(data.elements.nodesLefthandMenuCta).contains('Nodes').click()
             cy.get(data.elements.typography).contains(data.content.title)
             cy.get(data.elements.typography).contains(data.content['sub-title'])
+            cy.wait(500)
             cy.get(data.elements.accordionProtocolName).contains(data.content.eth_protocolName).click()
-            cy.get(data.elements.eth_accordionCta)
-                .find(data.elements.accordionChevron).click()
-            cy.get(data.elements.siteInput).contains(data.content.eth_site1)
-            cy.get(data.elements.siteInput).contains(data.content.eth_site2)
+            cy.get(data.elements.siteInput1).contains(data.content.eth_site1)
+            cy.get(data.elements.siteInput2).contains(data.content.eth_site2)
 
         })
 
