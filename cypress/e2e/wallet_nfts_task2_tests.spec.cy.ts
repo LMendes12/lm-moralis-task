7fc9016f05e2b3b2d807caca40386cd2468aa2f4
Lucia Mendes <luciasofiamendes@gmail.com>
2024-07-17 15:37:55 +0100

    added positive scenarios to test the wallet nfts api

--- cypress/e2e/wallet_nfts_task2_tests.spec.cy.ts
+++ cypress/e2e/wallet_nfts_task2_tests.spec.cy.ts
@@ -2,24 +2,111 @@
 
 //Load variables
 const apiAddress = Cypress.env("moralisWeb3API")
-const address = Cypress.env("address")
+const walletAddress = Cypress.env("walletAddress") //using the wallet address available on the moralis docs
 const web3ApiKey = Cypress.env("web3ApiKey")
+let tokenAddress = ''
 
-
-describe('Positive scenarios for the getWallet API', () => {
-    it('Get Results', () => {
+describe('Positive scenarios for the get NFTs by wallet API', () => {
+    it('Validate successful response from the api', () => {
         cy.request({
             method: 'GET',
-            url: `${apiAddress}/${address}/nft`,
+            url: `${apiAddress}/${walletAddress}/nft`,
             headers: {
                 'content-type': 'application/json',
                 'x-api-key': web3ApiKey
+            },
+            body: {
+                chain: 'eth',
+                format: 'decimal',
+                media_items: false
             }
         }).then((response: Cypress.Response<any>) => {
             expect(response.status).eq(200)
-            cy.log(JSON.stringify(response))
+            cy.log('The request was successful and returned status code: ' + response.status)
+            tokenAddress = response.body.result[0].token_address
+            cy.log('Token address to be used in next request is: ' + tokenAddress)
         })
-
     })
 
+    it('Validate only the selected token_addresses NFTs are in the response', () => {
+        cy.request({
+            method: 'GET',
+            url: `${apiAddress}/${walletAddress}/nft`,
+            headers: {
+                'content-type': 'application/json',
+                'x-api-key': web3ApiKey
+            },
+            qs: {
+                chain: 'eth',
+                format: 'decimal',
+                media_items: false,
+                token_addresses: [
+                    tokenAddress
+                ]
+            }
+        }).then((response: Cypress.Response<any>) => {
+            expect(response.status).eq(200)
+
+            //Validating the response is an array with only one result and the token address matches the request
+            expect(response.body).to.have.property('result').that.is.an('array')
+            expect(response.body.result).to.have.length(1)
+            expect(response.body.result[0].token_address).to.eq(tokenAddress)
+        })
+    })
+
+})
+
+describe('Positive scenarios for the get NFT collections by wallet', () => {
+    it('Validate get NFT collections by wallet successful response from the api', () => {
+        cy.request({
+            method: 'GET',
+            url: `${apiAddress}/${walletAddress}/nft/collections`,
+            headers: {
+                'content-type': 'application/json',
+                'x-api-key': web3ApiKey
+            },
+            qs: {
+                'total_count': false
+            }
+        }).then((response: Cypress.Response<any>) => {
+            expect(response.status).eq(200)
+            cy.log('The request was successful and returned status code: ' + response.status)
+
+        })
+    })   
+
+    it('Validate count is not displayed when token_counts is set to false', () => {
+        cy.request({
+            method: 'GET',
+            url: `${apiAddress}/${walletAddress}/nft/collections`,
+            headers: {
+                'content-type': 'application/json',
+                'x-api-key': web3ApiKey
+            },
+            qs: {
+                'total_count': false
+            }
+        }).then((response: Cypress.Response<any>) => {
+            expect(response.status).eq(200)
+            expect(response.body.result[0]).to.not.have.property('count')
+        })
+    })
+
+    it('Validate count is displayed when token_counts is set to true', () => {
+        cy.request({
+            method: 'GET',
+            url: `${apiAddress}/${walletAddress}/nft/collections`,
+            headers: {
+                'content-type': 'application/json',
+                'x-api-key': web3ApiKey
+            },
+            qs: {
+                token_counts: true
+            }
+        }).then((response: Cypress.Response<any>) => {
+            expect(response.status).eq(200)
+            expect(response.body.result[0]).to.have.property('count')
+
+        })
+    })
 })
